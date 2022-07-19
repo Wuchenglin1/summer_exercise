d0ef9290b5411d96b71cbf3e8ae4090a8d31f60e
Wuchenglin1 <1336636739@qq.com>
2022-07-19 15:54:54 +0800

    Exercise_3

--- Exercise_3/main.go
+++ Exercise_3/main.go
@@ -1,5 +1,20 @@
 package main
 
+import (
+	"fmt"
+	"log"
+	"summer/summer_exercise/Exercise_3/clock"
+	"time"
+)
+
 func main() {
-	test()
+	cron := clock.New(clock.WithSecond(true))
+	i, err := cron.AddFunction("1 * * * * *", func() {
+		fmt.Println("5s")
+	})
+	if err != nil {
+		log.Fatalf("error : %v", err)
+	}
+	fmt.Println(i)
+	time.Sleep(time.Second * 10)
 }
